c67bc180b73894ff821818f47dbea75bbedf748e
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-08-02 16:26:44 -0600

    Initializing Express backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend Server is running');
+});

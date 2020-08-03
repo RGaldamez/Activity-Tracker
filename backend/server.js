dd17719e3b9b785c25fae6234e4a032c9f798c16
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-08-02 21:26:56 -0600

    Setting up dotenv files and postgre connection with sequelize

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
+const db = require('./db/db');
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+db.authenticate()
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((error) => {
+    console.error('Connection to database failed', error.message);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);

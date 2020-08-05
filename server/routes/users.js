fdb3ac846c6411f93b80eb39f0f2bccd684b8f97
unknown <ricardo.j.galdamez@gmail.com>
2020-08-05 17:16:09 -0600

    Create new user in DB router added

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const db = require("../db/db");
 const router = express.Router();
+//user- email, role, date
+router.post("/user", async (req, res) => {
+  const { email, role } = req.body;
+  try {
+    const newUser = await db.query(
+      `INSERT INTO users(email,user_role) values('${email}','${role}') RETURNING *;`
+    );
+    res.send(newUser);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,38 +9,28 @@ const activitiesRoutes = require("./routes/activities");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 app.use(userRoutes);
 app.use(focusRoutes);
 app.use(categoriesRoutes);
 app.use(activitiesRoutes);
-
-app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
 db.query("SELECT NOW()", (err, res) => {
-  const { rows } = res;
-  const { now } = rows[0];
+  if (!err) {
+    const { rows } = res;
+    const { now } = rows[0];
 
-  console.log(`Connected to DB at ${now} `);
+    console.log(`Connected to DB at ${now} `);
+  } else {
+    console.log("Connection to DB failed", err);
+  }
 });
 
 app.get("/", (req, res) => {
   res.send("Backend Server is running");
 });
-
-// app.post("/user", async (req, res) => {
-//   try {
-//     const { email, role } = req.body;
-//     // console.log(email, role);
-//     const newUser = await db.query(
-//       `INSERT INTO users(email, role)
-//       VALUES ('${email}','${role}') RETURNING *;`
-//     );
-//     res.send(newUser);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// });

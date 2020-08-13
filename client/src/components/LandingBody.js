935689dde737ae22dce8fcce5a198ec1671580c6
unknown <ricardo.j.galdamez@gmail.com>
2020-08-12 18:59:45 -0600

    Adding get started button to sign up

--- client/src/components/LandingBody.js
+++ client/src/components/LandingBody.js
@@ -5,7 +5,14 @@ const LandingBody = () => {
   return (
     <div>
       <div className='landing-picture'>
-        <div className='landing-overlay'></div>
+        <div className='landing-overlay'>
+          <p className='d-none d-lg-block d-md-block main-header'>
+            Don't let your time manage you.
+          </p>
+          <div className='btn-header-container'>
+            <button className='btn btn-info btn-header'> Get Started </button>
+          </div>
+        </div>
       </div>
       {user ? <h1>Hello {user.name}</h1> : <h1>User not logged in</h1>}
     </div>
